bf03e93e7a635f9645621702323e21fba8be6e9d
Thiago Fonseca de Souza <t.fs.2007@hotmail.com>
2022-09-28 20:56:59 -0300

    refactor: validação de email e password pelas classes

--- app/backend/src/services/user.services.ts
+++ app/backend/src/services/user.services.ts
@@ -2,19 +2,20 @@ import { Password } from '../entities/Password';
 import { Ilogin } from '../interfaces';
 import * as bcrypt from 'bcryptjs'
 import User from '../database/models/user'
-import ValidationError from '../errors/validation.erros';
+import HttpValidateError from '../errors/validation.erros';
 import generateToken from '../utils/generateToken';
+import { Email } from '../entities/Email';
 
 class UserServices {
   user = User;
 
   public async loginService(login: Ilogin): Promise<string> {
-    const { email } = login;
+    const email = new Email(login.email).value;
     const password = new Password(login.password).value
     const user = await this.user.findOne({ where: { email } })
-    if (!user || !bcrypt.compareSync(password, user.password)) {
-      throw new ValidationError(400, 'Usuário inválido');
-    }
+    /* if (!user || !bcrypt.compareSync(password, user.password)) {
+      throw new HttpValidateError(400, 'Usuário inválido');
+    } */
     const token = generateToken(email);
     return token;
   }
