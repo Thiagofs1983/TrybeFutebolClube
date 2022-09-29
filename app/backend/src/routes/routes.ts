b6ac86911d15bf757617c64f2bef84dc2feef614
Thiago Fonseca de Souza <t.fs.2007@hotmail.com>
2022-09-29 13:00:09 -0300

    feat: rota get /teams para listar todos os times

--- app/backend/src/routes/routes.ts
+++ app/backend/src/routes/routes.ts
@@ -1,12 +1,16 @@
 import { Router } from 'express'
 import tokenValidation from '../middlewares/auth';
 import UserController from '../controllers/user.controllers';
+import TeamsControllers from '../controllers/teams.controllers';
 
 const userController = new UserController();
+const teamsController = new TeamsControllers();
 
 const router = Router();
 
 router.post('/login', userController.loginController);
 router.get('/login/validate', tokenValidation, userController.getUser);
+
+router.get('/teams', teamsController.getAll);
 
 export default router;
