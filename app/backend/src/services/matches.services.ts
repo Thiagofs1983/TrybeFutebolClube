e10a7d896be65bcedb93917f38efff156a629db7
Thiago Fonseca de Souza <t.fs.2007@hotmail.com>
2022-09-29 15:31:49 -0300

    feat: método para criar novos jogos

--- app/backend/src/services/matches.services.ts
+++ app/backend/src/services/matches.services.ts
@@ -1,15 +1,21 @@
 import Team from "../database/models/team";
 import Matche from "../database/models/matche";
+import { IMatches } from "../interfaces";
 
 class MatchesServices {
   matche = Matche;
 
-  public async matchesInProgress(inProgress: boolean) {
+  public async matchesInProgress(inProgress: boolean): Promise<Matche[]> {
     const matches = await this.matche.findAll({ where: { inProgress }, include: [
       { model: Team, as: 'teamHome', attributes: { exclude: ['id'] } },
       { model: Team, as: 'teamAway', attributes: { exclude: ['id'] } }
     ] })
     return matches;
+  }
+
+  public async createNewMatche(matche: IMatches) {
+    const newMatche = await this.matche.create({...matche, inProgress: true});
+    return newMatche;
   }
 }
 
