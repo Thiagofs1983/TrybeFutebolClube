fc23e6d452b7d4c86fd4fd68ddc88e5aaafa5967
Thiago Fonseca de Souza <t.fs.2007@hotmail.com>
2022-09-29 13:06:51 -0300

    feat: busca time pelo id informado

--- app/backend/src/services/teams.services.ts
+++ app/backend/src/services/teams.services.ts
@@ -1,3 +1,4 @@
+import HttpValidateError from "../errors/validation.erros";
 import Team from "../database/models/team";
 
 class TeamsServices {
@@ -6,6 +7,12 @@ class TeamsServices {
   public async getAll(): Promise<Team[]> {
     const teams = await this.team.findAll();
     return teams;
+  }
+
+  public async getById(id: number): Promise<Team> {
+    const team = await this.team.findByPk(id);
+    if (!team) throw new HttpValidateError(400, 'Team not found')
+    return team;
   }
 }
 
