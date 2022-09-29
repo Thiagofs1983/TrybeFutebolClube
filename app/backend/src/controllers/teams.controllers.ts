a9512cee4732836985a6c4abf57099f3a7770987
Thiago Fonseca de Souza <t.fs.2007@hotmail.com>
2022-09-29 12:58:58 -0300

    feat: classe TeamsControllers com o método getAll para listar todos os times na camada controllers

---
+++ app/backend/src/controllers/teams.controllers.ts
@@ -0,0 +1,13 @@
+import { Request, Response } from "express";
+import TeamsServices from "../services/teams.services";
+
+class TeamsControllers {
+  constructor(private team = new TeamsServices()) { };
+
+  public getAll = async (req: Request, res: Response): Promise<void> => {
+    const teams = await this.team.getAll();
+    res.status(200).json(teams);
+  }
+}
+
+export default TeamsControllers;
