e27d49cb9c3918d79bf6cde8ef758165a8b66e2a
Thiago Fonseca de Souza <t.fs.2007@hotmail.com>
2022-10-04 08:11:49 -0300

    test: teste para busca de todos os jogos cadastrados no BD

---
+++ app/backend/src/tests/matches.integration.test.ts
@@ -0,0 +1,67 @@
+import * as Sinon from 'sinon';
+import * as chai from 'chai';
+import * as jwt from 'jsonwebtoken';
+// @ts-ignore
+import chaiHttp = require('chai-http');
+
+import { app } from '../app';
+import Matche from '../database/models/matche';
+// import { IUser } from '../interfaces'
+
+import { Response } from 'superagent';
+
+chai.use(chaiHttp);
+
+const { expect } = chai;
+
+
+const mockMatches = [
+  {
+    id: 1,
+    homeTeam: 16,
+    homeTeamGoals: 1,
+    awayTeam: 8,
+    awayTeamGoals: 1,
+    inProgress: false,
+    teamHome: {
+      teamName: "São Paulo"
+    },
+    teamAway: {
+      teamName: "Grêmio"
+    }
+  },
+  {
+    id: 2,
+    homeTeam: 9,
+    homeTeamGoals: 1,
+    awayTeam: 14,
+    awayTeamGoals: 1,
+    inProgress: true,
+    teamHome: {
+      teamName: "Internacional"
+    },
+    teamAway: {
+      teamName: "Santos"
+    }
+  },
+]
+
+describe('teste da rota /matches', () => {
+  describe('GET', () => {
+    describe('Busca todos os jogos', () => {
+      before(() => {
+        Sinon.stub(Matche, 'findAll').resolves(mockMatches as any)
+      });
+  
+      after(() => {
+        Sinon.restore();
+      })
+  
+      it('Retorna todos os jogos cadastrados no BD', async () => {
+        const response = await chai.request(app).get('/matches');
+        expect(response.status).to.equal(200);
+        expect(response.body).to.deep.equal(mockMatches);
+      });
+    });
+  });
+});
