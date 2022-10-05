55965cd4eab550832a98015a9578a7e77806295c
Thiago Fonseca de Souza <t.fs.2007@hotmail.com>
2022-10-05 14:33:38 -0300

    test: teste para o endpoint leaderboards que retorna toda a classificação

--- app/backend/src/tests/leaderbord.integration.test.ts
+++ app/backend/src/tests/leaderbord.integration.test.ts
@@ -1032,6 +1032,201 @@ const mockLeaderboardAway = [
     }
   ];
 
+const mockLeaderboard = [
+  {
+    "name": "Palmeiras",
+    "totalPoints": 13,
+    "totalGames": 5,
+    "totalVictories": 4,
+    "totalDraws": 1,
+    "totalLosses": 0,
+    "goalsFavor": 17,
+    "goalsOwn": 5,
+    "goalsBalance": 12,
+    "efficiency": "86.67"
+  },
+  {
+    "name": "Corinthians",
+    "totalPoints": 12,
+    "totalGames": 5,
+    "totalVictories": 4,
+    "totalDraws": 0,
+    "totalLosses": 1,
+    "goalsFavor": 12,
+    "goalsOwn": 3,
+    "goalsBalance": 9,
+    "efficiency": "80.00"
+  },
+  {
+    "name": "Santos",
+    "totalPoints": 11,
+    "totalGames": 5,
+    "totalVictories": 3,
+    "totalDraws": 2,
+    "totalLosses": 0,
+    "goalsFavor": 12,
+    "goalsOwn": 6,
+    "goalsBalance": 6,
+    "efficiency": "73.33"
+  },
+  {
+    "name": "Grêmio",
+    "totalPoints": 10,
+    "totalGames": 5,
+    "totalVictories": 3,
+    "totalDraws": 1,
+    "totalLosses": 1,
+    "goalsFavor": 9,
+    "goalsOwn": 8,
+    "goalsBalance": 1,
+    "efficiency": "66.67"
+  },
+  {
+    "name": "Internacional",
+    "totalPoints": 10,
+    "totalGames": 5,
+    "totalVictories": 3,
+    "totalDraws": 1,
+    "totalLosses": 1,
+    "goalsFavor": 7,
+    "goalsOwn": 6,
+    "goalsBalance": 1,
+    "efficiency": "66.67"
+  },
+  {
+    "name": "Real Brasília",
+    "totalPoints": 10,
+    "totalGames": 5,
+    "totalVictories": 3,
+    "totalDraws": 1,
+    "totalLosses": 1,
+    "goalsFavor": 5,
+    "goalsOwn": 4,
+    "goalsBalance": 1,
+    "efficiency": "66.67"
+  },
+  {
+    "name": "São Paulo",
+    "totalPoints": 8,
+    "totalGames": 5,
+    "totalVictories": 2,
+    "totalDraws": 2,
+    "totalLosses": 1,
+    "goalsFavor": 9,
+    "goalsOwn": 6,
+    "goalsBalance": 3,
+    "efficiency": "53.33"
+  },
+  {
+    "name": "Ferroviária",
+    "totalPoints": 7,
+    "totalGames": 6,
+    "totalVictories": 2,
+    "totalDraws": 1,
+    "totalLosses": 3,
+    "goalsFavor": 7,
+    "goalsOwn": 8,
+    "goalsBalance": -1,
+    "efficiency": "38.89"
+  },
+  {
+    "name": "São José-SP",
+    "totalPoints": 6,
+    "totalGames": 5,
+    "totalVictories": 2,
+    "totalDraws": 0,
+    "totalLosses": 3,
+    "goalsFavor": 5,
+    "goalsOwn": 6,
+    "goalsBalance": -1,
+    "efficiency": "40.00"
+  },
+  {
+    "name": "Flamengo",
+    "totalPoints": 5,
+    "totalGames": 5,
+    "totalVictories": 1,
+    "totalDraws": 2,
+    "totalLosses": 2,
+    "goalsFavor": 2,
+    "goalsOwn": 5,
+    "goalsBalance": -3,
+    "efficiency": "33.33"
+  },
+  {
+    "name": "Minas Brasília",
+    "totalPoints": 5,
+    "totalGames": 6,
+    "totalVictories": 1,
+    "totalDraws": 2,
+    "totalLosses": 3,
+    "goalsFavor": 5,
+    "goalsOwn": 9,
+    "goalsBalance": -4,
+    "efficiency": "27.78"
+  },
+  {
+    "name": "Cruzeiro",
+    "totalPoints": 4,
+    "totalGames": 5,
+    "totalVictories": 1,
+    "totalDraws": 1,
+    "totalLosses": 3,
+    "goalsFavor": 8,
+    "goalsOwn": 10,
+    "goalsBalance": -2,
+    "efficiency": "26.67"
+  },
+  {
+    "name": "Avaí/Kindermann",
+    "totalPoints": 4,
+    "totalGames": 5,
+    "totalVictories": 1,
+    "totalDraws": 1,
+    "totalLosses": 3,
+    "goalsFavor": 4,
+    "goalsOwn": 8,
+    "goalsBalance": -4,
+    "efficiency": "26.67"
+  },
+  {
+    "name": "Botafogo",
+    "totalPoints": 4,
+    "totalGames": 5,
+    "totalVictories": 1,
+    "totalDraws": 1,
+    "totalLosses": 3,
+    "goalsFavor": 3,
+    "goalsOwn": 8,
+    "goalsBalance": -5,
+    "efficiency": "26.67"
+  },
+  {
+    "name": "Bahia",
+    "totalPoints": 2,
+    "totalGames": 5,
+    "totalVictories": 0,
+    "totalDraws": 2,
+    "totalLosses": 3,
+    "goalsFavor": 2,
+    "goalsOwn": 6,
+    "goalsBalance": -4,
+    "efficiency": "13.33"
+  },
+  {
+    "name": "Napoli-SC",
+    "totalPoints": 2,
+    "totalGames": 5,
+    "totalVictories": 0,
+    "totalDraws": 2,
+    "totalLosses": 3,
+    "goalsFavor": 3,
+    "goalsOwn": 12,
+    "goalsBalance": -9,
+    "efficiency": "13.33"
+  }
+];
+
 describe('teste da rota /leaderboard/home', () => {
   describe('GET', () => {
     describe('Mostra a classificação dos times', () => {
@@ -1058,6 +1253,14 @@ describe('teste da rota /leaderboard/home', () => {
       });
       it('Retorna o status 200 para times visitantes', async () => {
         const response = await chai.request(app).get('/leaderboard/away');
+        expect(response.status).to.equal(200);
+      });
+      it('Retorna a classificação geral dos times', async () => {
+        const response = await chai.request(app).get('/leaderboard');
+        expect(response.body).to.deep.equal(mockLeaderboard);
+      });
+      it('Retorna o status 200 para a classificação geral dos times', async () => {
+        const response = await chai.request(app).get('/leaderboard');
         expect(response.status).to.equal(200);
       });
     });
