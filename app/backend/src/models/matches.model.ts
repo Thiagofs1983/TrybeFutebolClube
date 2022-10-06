80cc1f60dac7ff93b83337a7b9f0c103e86cec7f
Thiago Fonseca de Souza <t.fs.2007@hotmail.com>
2022-10-06 08:10:25 -0300

    refactor: elimina a comunicação da camada services de matches com o banco de dados

---
+++ app/backend/src/models/matches.model.ts
@@ -0,0 +1,61 @@
+import { Op } from 'sequelize';
+import Team from '../database/models/team';
+import Matche from '../database/models/matche';
+import { IMatches, ISimpleMatches } from '../interfaces';
+import CustomError from '../errors/custom.erros';
+
+class MatchModel {
+  constructor(private match = Matche, private team = Team) { }
+
+  public async findAll() {
+    const matches = await this.match.findAll({
+      include: [
+        { model: Team, as: 'teamHome', attributes: { exclude: ['id'] } },
+        { model: Team, as: 'teamAway', attributes: { exclude: ['id'] } },
+      ],
+    });
+    return matches;
+  }
+
+  public async findAllMatchesInProgress(inProgress: boolean) {
+    const matches = await this.match.findAll(
+      {
+        where: { inProgress },
+        include: [
+          { model: Team, as: 'teamHome', attributes: { exclude: ['id'] } },
+          { model: Team, as: 'teamAway', attributes: { exclude: ['id'] } },
+        ] },
+    );
+    return matches;
+  }
+
+  public async findAllCreate(match: IMatches) {
+    const teamExists = await this.team
+      .findAll({ where: { [Op.or]: [{ id: match.homeTeam }, { id: match.awayTeam }] } });
+    if (teamExists.length !== 2) throw new CustomError(404, 'There is no team with such id!');
+  }
+
+  public async createNewMatche(match: IMatches) {
+    const newMatche = await this.match.create({ ...match, inProgress: true });
+
+    return newMatche;
+  }
+
+  public async updateInProgress(id: number) {
+    const [matche] = await this.match.update(
+      { inProgress: false },
+      { where: { id } },
+    );
+    if (matche === 0) throw new CustomError(400, 'It is not possible to update a match');
+  }
+
+  public async updateMatches(date: ISimpleMatches, id: number): Promise<void> {
+    const [match] = await this.match.update(
+      { awayTeamGoals: date.awayTeamGoals, homeTeamGoals: date.homeTeamGoals },
+      { where: { id } },
+    );
+    if (match === 0) throw new CustomError(400, 'It is not possible to update a match');
+  }
+}
+
+export default MatchModel;
--- app/backend/src/services/matches.services.ts
+++ app/backend/src/services/matches.services.ts
@@ -1,61 +1,35 @@
-import { Op } from 'sequelize';
-import Team from '../database/models/team';
-import Matche from '../database/models/matche';
 import { IMatches, ISimpleMatches } from '../interfaces';
-import CustomError from '../errors/custom.erros';
 import Matches from '../entities/Matches';
+import MatchModel from '../models/matches.model';
 
 class MatchesServices {
-  matche = Matche;
-  team = Team;
+  constructor(private match = new MatchModel()) { }
 
-  public async getAll() {
-    const matches = await this.matche.findAll({
-      include: [
-        { model: Team, as: 'teamHome', attributes: { exclude: ['id'] } },
-        { model: Team, as: 'teamAway', attributes: { exclude: ['id'] } },
-      ],
-    });
+  public async getAll(): Promise<IMatches[]> {
+    const matches = await this.match.findAll();
     return matches;
   }
 
-  public async matchesInProgress(inProgress: boolean): Promise<Matche[]> {
-    const matches = await this.matche.findAll(
-      {
-        where: { inProgress },
-        include: [
-          { model: Team, as: 'teamHome', attributes: { exclude: ['id'] } },
-          { model: Team, as: 'teamAway', attributes: { exclude: ['id'] } },
-        ] },
-    );
+  public async matchesInProgress(inProgress: boolean): Promise<IMatches[]> {
+    const matches = await this.match.findAllMatchesInProgress(inProgress);
     return matches;
   }
 
-  public async createNewMatche(matche: IMatches): Promise<Matche> {
+  public async createNewMatche(matche: IMatches): Promise<IMatches> {
     const { match } = new Matches(matche);
-    const teamExists = await this.team
-      .findAll({ where: { [Op.or]: [{ id: match.homeTeam }, { id: match.awayTeam }] } });
-    if (teamExists.length !== 2) throw new CustomError(404, 'There is no team with such id!');
+    this.match.findAllCreate(match);
 
-    const newMatche = await this.matche.create({ ...match, inProgress: true });
+    const newMatche = await this.match.createNewMatche(match);
 
     return newMatche;
   }
 
   public async updateInProgress(id: number): Promise<void> {
-    const [matche] = await this.matche.update(
-      { inProgress: false },
-      { where: { id } },
-    );
-    if (matche === 0) throw new CustomError(400, 'It is not possible to update a match');
+    await this.match.updateInProgress(id);
   }
 
   public async updateMatches(date: ISimpleMatches, id: number): Promise<void> {
-    const [match] = await this.matche.update(
-      { awayTeamGoals: date.awayTeamGoals, homeTeamGoals: date.homeTeamGoals },
-      { where: { id } },
-    );
-    if (match === 0) throw new CustomError(400, 'It is not possible to update a match');
+    await this.match.updateMatches(date, id);
   }
 }
 
